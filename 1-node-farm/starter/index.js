315c2f7ee25476af8d1faccf34aa5bc527c24324
Will <wfoster12@gmail.com>
2019-09-28 12:35:10 +0100

    first server

--- 1-node-farm/starter/index.js
+++ 1-node-farm/starter/index.js
@@ -11,22 +11,32 @@ const fs = require('fs');
 // console.log('File created sucessfully');
 
 //Non-blocking, asynchronous way
-fs.readFile('./txt/start.txt', 'utf-8', (err,data1)=> {
-    if (err) {
-        console.log("ERROR");
-        return
-    }
+// fs.readFile('./txt/start.txt', 'utf-8', (err,data1)=> {
+//     if (err) {
+//         console.log("ERROR");
+//         return
+//     }
     
-    fs.readFile(`./txt/${data1}.txt`, 'utf-8', (err,data2)=> {
-        console.log(data2);
-        fs.readFile(`./txt/append.txt`, 'utf-8', (err,data3)=> {
-            console.log(data3);
-            fs.writeFile('./txt/final.txt', `${data2}\n${data3}`, 'utf-8',err => {
-                console.log("File created");
-            })
-        })
+//     fs.readFile(`./txt/${data1}.txt`, 'utf-8', (err,data2)=> {
+//         console.log(data2);
+//         fs.readFile(`./txt/append.txt`, 'utf-8', (err,data3)=> {
+//             console.log(data3);
+//             fs.writeFile('./txt/final.txt', `${data2}\n${data3}`, 'utf-8',err => {
+//                 console.log("File created");
+//             })
+//         })
 
-    })
-})
+//     })
+// })
 
 
+//SERVER
+
+const http = require('http');
+const server = http.createServer((req, res)=>{
+    res.end('Server response');
+});
+
+server.listen(8000, '127.0.0.1', () =>{
+    console.log('listening to requests on port 8000');
+});
