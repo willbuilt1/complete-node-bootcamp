f0a5d55d1c86fb5067bf254385e7a95d87409413
Will <wfoster12@gmail.com>
2019-10-01 21:44:58 +0100

    First server write up

--- 1-node-farm/starter/index.js
+++ 1-node-farm/starter/index.js
@@ -32,8 +32,10 @@ const fs = require('fs');
 
 //SERVER
 
-//sync method this does not block other lines as only executed once on load
+//funtion that takes the template and the array object. Then replaces the {%placeholder%} with the relevant piece of data 
 const replaceTemplate = (template, product) =>{
+    //Template provided is a string so has replace method
+    //REGEX used so that it replaces every instance of requested string
     let output = template.replace(/{%PRODUCTNAME%}/g, product.productName);
     output = output.replace(/{%QUANTITY%}/g, product.quantity);
     output = output.replace(/{%IMAGE%}/g, product.image);
@@ -47,26 +49,33 @@ const replaceTemplate = (template, product) =>{
     return output;
 }
 
+//sync method this does not block other lines as only executed once on load
+//returns file as a string
 const tempOverview =  fs.readFileSync(`${__dirname}/templates/overview.html`, 'utf-8');
 const tempProduct=  fs.readFileSync(`${__dirname}/templates/product.html`, 'utf-8');
 const tempCard=  fs.readFileSync(`${__dirname}/templates/card.html`, 'utf-8');
 
+//data returns as a string, this is parsed so that it can be accessed by javascript
 const data =  fs.readFileSync(`${__dirname}/dev-data/data.json`, 'utf-8')
 const dataObj = JSON.parse(data);
 
 const http = require('http');
 const url = require('url');
 const server = http.createServer((req, res)=>{
-    //console.log(url.parse(req.url, true));
+    // Destructures returned object and creates consts query and pathname with values equal from the object
     const {query, pathname} = url.parse(req.url, true)
 
 
 //Overview page
     if (pathname === '/' || pathname === '/overview'){
+        //gives response code of 200 which is a success and says that html will be output
         res.writeHead(200, {
             'content-type':'text/html'
         })
+        //Firstly returns an array with template card with placeholders replaced with info from el(dataobj[i])
+        //This is then joined to return one string with all html
         const cardsHtml = dataObj.map(el => replaceTemplate(tempCard, el)).join('')
+        // cards then put into temp overview
         const output = tempOverview.replace('{%PRODUCT_CARDS%}', cardsHtml)
         res.end(output);
 //Product page
@@ -74,7 +83,9 @@ const server = http.createServer((req, res)=>{
         res.writeHead(200, {
             'content-type':'text/html'
         })
+        //gets item that has been clicked on 
         const product = dataObj[query.id];
+        //uses this to put data into product card
         const output = replaceTemplate(tempProduct, product);
         res.end(output);
 //API      
@@ -83,12 +94,14 @@ const server = http.createServer((req, res)=>{
     
         // fs.readFile(`${__dirname}/dev-data/data.json`, 'utf-8', (err, data) =>{
         //     const productData = JSON.parse(data);
+        //to show api data
             res.writeHead(200, {
                 'content-type':'application/json'
             })
             res.end(data);
         // })
 //Not found  
+// outputs 404 code and not found message to page
     } else {
         res.writeHead(404, {
             'Content-type': 'text/html'
