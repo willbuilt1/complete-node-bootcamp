67680624f903214eefe2c61eeeb6e6371ee00f65
Will <wfoster12@gmail.com>
2019-10-18 21:47:13 +0100

    Refactoring and creating own middleware

--- 4-natours/starter/app.js
+++ 4-natours/starter/app.js
@@ -3,32 +3,25 @@ const express = require('express');
 
 const app = express();
 
-//Testing writing own middleware
-// const myConfirm = (req, res, next) => {
-//     console.log('MIDDLEWARE!!');
-//     next();
-// }
+// Testing writing own middleware
+const timeRequested = (req, res, next) => {
+    req.timeRequested = new Date().toISOString();
+
+    next();
+}
 
 //Using middleware
 // app.use(myConfirm);
-app.use(express.json())
+app.use(express.json());
+app.use(timeRequested)
 
-// app.get('/', (req, res) => {
-//     res.status(200).json({
-//         message: 'Server on...',
-//         app: 'natours'
-//     })
-// })
-
-// app.post('/', (req, res) => {
-//     res.send('You can post to this endpoint...')
-// })
 //Reading data from JSON file - READ 
 const tours = JSON.parse(fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`));
-//Returning all data
-app.get('/api/v1/tours', (req, res) => {
+
+const getTours = (req, res) => {
     res.status(200).json({
         status: 'success',
+        timeRequested: req.timeRequested,
         results: tours.length,
         data: {
             //key matches endpoint value is the data returned
@@ -36,9 +29,9 @@ app.get('/api/v1/tours', (req, res) => {
             tours // this is equivalent as key and value are the same
         }
     })
-})
-//returning a particular tour
-app.get('/api/v1/tours/:id', (req, res) => {
+}
+
+const getTour = (req, res) => {
     // Get the ID requested from the pram
     const id = parseInt(req.params.id); // alternatively * 1
     //find the tour with the id == id requested
@@ -58,10 +51,9 @@ app.get('/api/v1/tours/:id', (req, res) => {
             tour
         }
     })
-})
+}
 
-// Creating a new tour
-app.post('/api/v1/tours', (req, res) => {
+const createTour = (req, res) => {
     //console.log(req.body);
 
     const newID = tours[tours.length - 1].id + 1
@@ -81,7 +73,64 @@ app.post('/api/v1/tours', (req, res) => {
             console.log(err);
         }
     })
-})
+}
+
+const updateTour = (req, res) => {
+    // Get the ID requested from the pram
+    const id = parseInt(req.params.id); // alternatively * 1
+    //find the tour with the id == id requested
+    const tour = tours.find(el => el.id === id)
+
+    //If no tour with that id (tour will be undefined) then return a fail
+    if (!tour) {
+        return res.status(404).json({
+            status: 'fail',
+            message: 'Invalid ID'
+        })
+    }
+    //Otherwise return requested data
+    res.status(200).json({
+        status: 'success',
+        data: {
+            tour: "Updated tour goes here"
+        }
+    })
+}
+
+const deleteTour = (req, res) => {
+    // Get the ID requested from the pram
+    const id = parseInt(req.params.id); // alternatively * 1
+    //find the tour with the id == id requested
+    const tour = tours.find(el => el.id === id)
+
+    //If no tour with that id (tour will be undefined) then return a fail
+    if (!tour) {
+        return res.status(404).json({
+            status: 'fail',
+            message: 'Invalid ID'
+        })
+    }
+    //Otherwise return requested data
+    res.status(204).json({
+        status: 'success',
+        data: null
+    })
+}
+
+// app.get('/api/v1/tours', getTours)
+// app.get('/api/v1/tours/:id', getTour)
+// app.post('/api/v1/tours', createTour)
+// app.patch('/api/v1/tours/:id', updateTour)
+// app.delete('/api/v1/tours/:id', deleteTour)
+
+app.route('/api/v1/tours')
+    .get(getTours)
+    .post(createTour);
+
+app.route('/api/v1/tours/:id')
+    .get(getTour)
+    .patch(updateTour)
+    .delete(deleteTour)
 
 const port = 3000;
 app.listen(port, () => {
